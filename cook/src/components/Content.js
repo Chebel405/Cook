4bda3193fba32e53f90ab4aa9a4ebc02c7259abe
Chebel405 <anne.chebel@gmail.com>
2023-09-21 14:04:57 +0200

    Méthode de filtre pour les recherches de l'utilisateur

--- cook/src/components/Content.js
+++ cook/src/components/Content.js
@@ -1,28 +1,50 @@
+import { useState } from 'react';
 import styles from './Content.module.scss';
+import Recipe from './Recipe';
+import { data } from './data/recipes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
 
  
 
 function Content() {
+  const recipes = data;
+  const[filter, setFilter] = useState('');
+
+  /**
+   * Fonction permettant de filtrer les recherches de l'utilisateur
+   * @param {*} e récupere l'evenement, la valeur de l'input
+   * setFilter va filtrer la demande tapée.
+   */
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
+
+ 
+
   return (
-<div className="flex-fill container p-20">
-<h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-<div className={`card p-20 ${styles.contentCard}`}>
-<div className={styles.grid}>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-<div className={styles.elem}>Element</div>
-</div>
-</div>
-</div>
-  );
-}
+          <div className="flex-fill container p-20">
+            <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+            <div className={`card d-flex-column p-20 ${styles.contentCard}`}>
+              <div className={ `d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}` }>  
+                <FontAwesomeIcon icon={faSearch} className="fa-magnifying-glass m-15" />
+                <input onInput={ handleInput } className='flex-fill'type='text'placeholder='Recherche' />
+              </div>
+
+              <div className={styles.grid}>
+                {recipes
+                .filter( r => r.title.toLowerCase().startsWith(filter))
+                .map((r) => (
+                  <Recipe key={r._id} title={r.title} image={r.image} />
+                ))}
+              </div>
+            </div>
+          </div>
+        );
+      }
 
  
 
---
+++ cook/src/components/Recipe.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import styles from './Recipe.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+ 
+
+function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+
+ 
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
+ 
+
+  return (
+    <div onClick={handleClick} className={styles.recipe}>
+      <div className={styles.imageContainer}>
+        <img src={image} alt={title} />
+      </div>
+      <div
+          className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
+      >
+      <h3 className="mb-10">{title}</h3>
+        <i className={`fa-solid fa-heart ${liked ? 'text-primary' : ''}`}></i>
+        <FontAwesomeIcon icon={faHeart} />
+    
+      </div>
+  </div>
+  );
+}
+
+ 
+
+export default Recipe;
