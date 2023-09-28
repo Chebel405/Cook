f42e238b35dded7ebea777d00864f181f865606d
Chebel405 <anne.chebel@gmail.com>
2023-09-28 19:58:38 +0200

    Limiter par page 18 recettes affichées

--- cook/src/pages/Homepage/Homepage.js
+++ cook/src/pages/Homepage/Homepage.js
@@ -14,6 +14,7 @@ function Homepage() {
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
   const [filter, setFilter] = useState('');
+  const [page, setPage] = useState(1);
   const BASE_URL_API = useContext(ApiContext)
 
   useEffect(() => {
@@ -21,10 +22,11 @@ function Homepage() {
     async function fetchRecipes() {
       try {
         setIsLoading(true);
-        const response = await fetch(BASE_URL_API);
+        // 18 articles par page
+        const response = await fetch(`${BASE_URL_API}?skip=${ (page - 1) * 18 }&limit=18`);
         if(response.ok && !cancel){
-          const recipes = await response.json();
-          setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
+          const newRecipes = await response.json();
+          setRecipes(x => Array.isArray(newRecipes) ? [...x, ...newRecipes] : [...x, newRecipes]);
         }
       }catch(e){
         console.log('ERREUR');
@@ -36,7 +38,7 @@ function Homepage() {
     } 
     fetchRecipes();
     return () => (cancel = true);
-  }, [BASE_URL_API]);
+  }, [BASE_URL_API, page]);
 
   // Recupere le recipe
   function updateRecipe(uptatedRecipe) {
@@ -55,17 +57,15 @@ function Homepage() {
     setFilter(filter.trim().toLowerCase());
   }
 
- 
-
   return (
           <div className="flex-fill container d-flex flex-column p-20">
-            <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+            <h1 className="my-30">Découvrez nos nouvelles recettes<small className={styles.small}>-{recipes.length}</small></h1>
             <div className={`card flex-fill d-flex-column p-20 mb-20 ${styles.contentCard}`}>
               <div className={ `d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}` }>  
                 <FontAwesomeIcon icon={faSearch} className="fa-magnifying-glass m-15" />
                 <input onInput={ handleInput } className='flex-fill'type='text'placeholder='Recherche' />
               </div>
-              { isLoading ? (
+              { isLoading && !recipes.length ? (
                 <Loading/> ) : (
               <div className={styles.grid}>
                 {recipes
@@ -75,6 +75,13 @@ function Homepage() {
                 ))}
               </div> 
               )}
+              <div className='d-flex flex-row justify-content-center align-items-center p-20'>
+                <button 
+                  onClick={() => setPage(page + 1) }
+                  className='btn btn-primary'>
+                  Charger plus de recettes
+                </button>
+              </div>
             </div>
           </div>
         );
