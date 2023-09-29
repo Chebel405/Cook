c3b58f235201ca4b85498f5236f74d19dc4924a2
Chebel405 <anne.chebel@gmail.com>
2023-09-29 13:16:43 +0200

    Rangement du code

--- cook/src/pages/Homepage/Homepage.js
+++ cook/src/pages/Homepage/Homepage.js
@@ -2,44 +2,21 @@ import { useContext, useEffect, useState } from 'react';
 import styles from './Homepage.module.scss';
 import Recipe from './components/Recipe/Recipe';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-//import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Loading from '../../components/Loading/Loading';
 import { ApiContext } from "../../context/ApiContext";
 import Search from './components/Search/Search';
-
+import { useFetchData } from '../../hooks/useFetchData';
 
 
 
 function Homepage() {
-  const [recipes, setRecipes] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
-  const [filter, setFilter] = useState('');
+ 
+  const [filter, setFilter] = useState("");
   const [page, setPage] = useState(1);
   const BASE_URL_API = useContext(ApiContext)
-
-  useEffect(() => {
-    let cancel = false;
-    async function fetchRecipes() {
-      try {
-        setIsLoading(true);
-        // 18 articles par page
-        const response = await fetch(`${BASE_URL_API}?skip=${ (page - 1) * 18 }&limit=18`);
-        if(response.ok && !cancel){
-          const newRecipes = await response.json();
-          setRecipes(x => Array.isArray(newRecipes) ? [...x, ...newRecipes] : [...x, newRecipes]);
-        }
-      }catch(e){
-        console.log('ERREUR');
-      } finally {
-        if(!cancel){
-          setIsLoading(false);
-        }    
-      }
-    } 
-    fetchRecipes();
-    return () => (cancel = true);
-  }, [BASE_URL_API, page]);
+  const [[recipes, setRecipes], isLoading] = useFetchData(BASE_URL_API, page);
+  
 
   // Recupere le recipe
   function updateRecipe(uptatedRecipe) {
@@ -47,8 +24,6 @@ function Homepage() {
       recipes.map((r) => (r._id === uptatedRecipe._id ? uptatedRecipe : r ))
     );
   }
-
-  
 
   return (
           <div className="flex-fill container d-flex flex-column p-20">
@@ -76,7 +51,5 @@ function Homepage() {
           </div>
         );
       }
-
- 
 
 export default Homepage;
